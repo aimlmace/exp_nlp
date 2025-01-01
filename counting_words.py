3e57b2ddbcc91a3eea6f0407f1cd4b14bbc00a1b
haanoon <hanoonmavoor@gmail.com>
2025-01-01 10:36:14 +0530

    second experiment

---
+++ counting_words.py
@@ -0,0 +1,23 @@
+import nltk
+import re
+from nltk.tokenize import word_tokenize
+
+with open ('data.txt','r') as file:
+	data = file.read()
+
+pattern = r'[^\w\s]|https?://\S+|www\.\S+'
+punctuation_removed = re.sub(pattern = pattern,repl = '', string = data)
+
+tokens = word_tokenize(punctuation_removed)
+
+print(f'Total Number of Tokens: {len(tokens)}')
+
+set_tokens = set(tokens)
+
+print(f'Total Number of Unique Tokens: {len(set_tokens)}')
+
+for word in set_tokens:
+	print(f'{word} occur {tokens.count(word)} times')
+	print(f'Percentage: {tokens.count(word)/len(tokens)*100:.2f}')
+print()
+print(f'TTR: {(len(set_tokens)/len(tokens))*100:.2f}')
